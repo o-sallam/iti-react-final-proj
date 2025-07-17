1fc1cbf28b560631ab4f8750b32f727c2e5a7226
o-sallam <omar.sallam.dev@gmail.com>
2025-07-17 05:09:33 +0200

    style(logo)

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -183,20 +183,32 @@ const SideBar = ({ sidebarToggle }) => {
         }`}
       >
         <Link to="/">
-          <span className={`logo ${sidebarToggle ? 'hidden' : ''}`}>
-            <img className="dark:hidden" src="/images/logo/logo.svg" alt="Logo" />
-            <img
-              className="hidden dark:block"
-              src="/images/logo/logo-dark.svg"
-              alt="Logo"
-            />
+          <span className={`logo ${sidebarToggle ? 'hidden' : ''} relative flex items-center`}>
+            {/* Custom SVG Logo */}
+            <span className="relative flex items-center">
+              <svg width="48" height="48" viewBox="-4.8 -4.8 57.60 57.60" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ minWidth: 48, minHeight: 48 }}>
+                <g id="SVGRepo_bgCarrier" strokeWidth="0" transform="translate(1.44,1.44), scale(0.94)"><rect x="-4.8" y="-4.8" width="57.60" height="57.60" rx="0" fill="#2F88FF" /></g>
+                <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" stroke="#fffafa" strokeWidth="1.824">
+                  <rect width="48" height="48" fill="white" fillOpacity="0.01"></rect>
+                  <path d="M32.9037 13.9271C31.2464 17.1587 27.8814 19.3701 24 19.3701C20.1185 19.3701 16.7536 17.1587 15.0963 13.9271C11.3982 16.659 9 21.0494 9 26C9 26.8177 9.06543 27.6201 9.19135 28.4023C9.45807 28.381 9.72775 28.3701 9.99996 28.3701C15.5228 28.3701 20 32.8473 20 38.3701C20 39.0664 19.9288 39.746 19.7934 40.4021C21.128 40.7913 22.5397 41 24 41C25.4603 41 26.8719 40.7913 28.2066 40.4021C28.0711 39.746 28 39.0664 28 38.3701C28 32.8473 32.4771 28.3701 38 28.3701C38.2722 28.3701 38.5419 28.381 38.8087 28.4023C38.9346 27.6201 39 26.8177 39 26C39 21.0494 36.6017 16.659 32.9037 13.9271Z" fill="#ffffff" stroke="#2F88FF" strokeWidth="0.00048" strokeLinecap="round" strokeLinejoin="round"></path>
+                  <path fillRule="evenodd" clipRule="evenodd" d="M24 13C26.2091 13 28 11.2091 28 9C28 6.79086 26.2091 5 24 5C21.7909 5 20 6.79086 20 9C20 11.2091 21.7909 13 24 13Z" fill="#ffffff" stroke="#2F88FF" strokeWidth="0.00048" strokeLinecap="round" strokeLinejoin="round"></path>
+                  <path fillRule="evenodd" clipRule="evenodd" d="M9 43C11.2091 43 13 41.2091 13 39C13 36.7909 11.2091 35 9 35C6.79086 35 5 36.7909 5 39C5 41.2091 6.79086 43 9 43Z" fill="#ffffff" stroke="#2F88FF" strokeWidth="0.00048" strokeLinecap="round" strokeLinejoin="round"></path>
+                  <path fillRule="evenodd" clipRule="evenodd" d="M39 43C41.2091 43 43 41.2091 43 39C43 36.7909 41.2091 35 39 35C36.7909 35 35 36.7909 35 39C35 41.2091 36.7909 43 39 43Z" fill="#ffffff" stroke="#2F88FF" strokeWidth="0.00048" strokeLinecap="round" strokeLinejoin="round"></path>
+                </g>
+                <g id="SVGRepo_iconCarrier">
+                  <rect width="48" height="48" fill="white" fillOpacity="0.01"></rect>
+                  <path d="M32.9037 13.9271C31.2464 17.1587 27.8814 19.3701 24 19.3701C20.1185 19.3701 16.7536 17.1587 15.0963 13.9271C11.3982 16.659 9 21.0494 9 26C9 26.8177 9.06543 27.6201 9.19135 28.4023C9.45807 28.381 9.72775 28.3701 9.99996 28.3701C15.5228 28.3701 20 32.8473 20 38.3701C20 39.0664 19.9288 39.746 19.7934 40.4021C21.128 40.7913 22.5397 41 24 41C25.4603 41 26.8719 40.7913 28.2066 40.4021C28.0711 39.746 28 39.0664 28 38.3701C28 32.8473 32.4771 28.3701 38 28.3701C38.2722 28.3701 38.5419 28.381 38.8087 28.4023C38.9346 27.6201 39 26.8177 39 26C39 21.0494 36.6017 16.659 32.9037 13.9271Z" fill="#ffffff" stroke="#2F88FF" strokeWidth="0.00048" strokeLinecap="round" strokeLinejoin="round"></path>
+                  <path fillRule="evenodd" clipRule="evenodd" d="M24 13C26.2091 13 28 11.2091 28 9C28 6.79086 26.2091 5 24 5C21.7909 5 20 6.79086 20 9C20 11.2091 21.7909 13 24 13Z" fill="#ffffff" stroke="#2F88FF" strokeWidth="0.00048" strokeLinecap="round" strokeLinejoin="round"></path>
+                  <path fillRule="evenodd" clipRule="evenodd" d="M9 43C11.2091 43 13 41.2091 13 39C13 36.7909 11.2091 35 9 35C6.79086 35 5 36.7909 5 39C5 41.2091 6.79086 43 9 43Z" fill="#ffffff" stroke="#2F88FF" strokeWidth="0.00048" strokeLinecap="round" strokeLinejoin="round"></path>
+                  <path fillRule="evenodd" clipRule="evenodd" d="M39 43C41.2091 43 43 41.2091 43 39C43 36.7909 41.2091 35 39 35C36.7909 35 35 36.7909 35 39C35 41.2091 36.7909 43 39 43Z" fill="#ffffff" stroke="#2F88FF" strokeWidth="0.00048" strokeLinecap="round" strokeLinejoin="round"></path>
+                </g>
+              </svg>
+              {/* Word behind the logo */}
+              <span class="text-gray-600 pr-4 text-3xl font-bold">
+                المحيط
+              </span>
+            </span>
           </span>
-
-          <img
-            className={`logo-icon ${sidebarToggle ? 'lg:block' : 'hidden'}`}
-            src="/images/logo/logo-icon.svg"
-            alt="Logo"
-          />
         </Link>
       </div>
       {/* SIDEBAR HEADER */}
